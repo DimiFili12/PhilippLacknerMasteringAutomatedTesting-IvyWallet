c1da3a22f922f974f97f0dfe6e3b60c59e7b51f2
DimiFili12 <dimifili1@gmail.com>
2026-04-03 13:38:37 +0300

    part 5.3 - solution

---
+++ core/domain/src/test/java/com/ivy/core/domain/action/exchange/SyncExchangeRatesActTest.kt
@@ -0,0 +1,54 @@
+package com.ivy.core.domain.action.exchange
+
+import assertk.assertThat
+import assertk.assertions.isNotNull
+import assertk.assertions.isNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class SyncExchangeRatesActTest {
+    private lateinit var syncExchangeRatesAct: SyncExchangeRatesAct
+    private lateinit var exchangeProviderFake: RemoteExchangeProviderFake
+    private lateinit var exchangeRateDaoFake: ExchangeRateDaoFake
+
+    @BeforeEach
+    fun setUp() {
+        exchangeProviderFake = RemoteExchangeProviderFake()
+        exchangeRateDaoFake = ExchangeRateDaoFake()
+        syncExchangeRatesAct = SyncExchangeRatesAct(
+            exchangeProvider = exchangeProviderFake,
+            exchangeRateDao = exchangeRateDaoFake
+        )
+    }
+
+    @Test
+    fun `Test sync exchange rates, negative values ignored`() = runBlocking {
+        syncExchangeRatesAct("USD")
+
+        val usdRate = exchangeRateDaoFake
+            .findAllByBaseCurrency("USD")
+            .first { it.isNotEmpty() }
+
+        val cadRate = usdRate.find { it.currency == "CAD" }
+
+        assertThat(cadRate).isNull()
+    }
+
+    @Test
+    fun `Test sync exchange rates, valid values saved`() = runBlocking<Unit> {
+        syncExchangeRatesAct("USD")
+
+        val usdRate = exchangeRateDaoFake
+            .findAllByBaseCurrency("USD")
+            .first { it.isNotEmpty() }
+
+        val eurRate = usdRate.find { it.currency == "EUR" }
+        val audRate = usdRate.find { it.currency == "AUD" }
+
+        assertThat(eurRate).isNotNull()
+        assertThat(audRate).isNotNull()
+    }
+}

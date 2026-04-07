ca303e57f950f7519b202214a6c907e606c28464
DimiFili12 <dimifili1@gmail.com>
2026-04-07 14:56:20 +0300

    part 7.6 - solution

---
+++ exchange-rates/src/test/java/com/ivy/exchangeRates/RatesDaoFake.kt
@@ -0,0 +1,27 @@
+package com.ivy.exchangeRates
+
+import com.ivy.core.persistence.algorithm.calc.Rate
+import com.ivy.core.persistence.algorithm.calc.RatesDao
+import com.ivy.data.CurrencyCode
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class RatesDaoFake: RatesDao {
+    var rates = MutableStateFlow(
+        listOf(
+            Rate(1.0, "EUR"),
+            Rate(1.2, "USD"),
+            Rate(1.8, "CAD")
+        )
+    )
+
+    private val overrides = MutableStateFlow(
+        listOf(
+            Rate(1.3, "USD")
+        )
+    )
+
+    override fun findAll(baseCurrency: CurrencyCode): Flow<List<Rate>> = rates
+
+    override fun findAllOverrides(baseCurrency: CurrencyCode): Flow<List<Rate>> = overrides
+}
---
+++ exchange-rates/src/test/java/com/ivy/exchangeRates/RatesStateFlowTest.kt
@@ -0,0 +1,71 @@
+package com.ivy.exchangeRates
+
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.doesNotContain
+import com.ivy.MainCoroutineExtension
+import com.ivy.core.domain.action.settings.basecurrency.BaseCurrencyFlow
+import com.ivy.core.persistence.algorithm.calc.Rate
+import com.ivy.exchangeRates.data.RateUi
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MainCoroutineExtension::class)
+class RatesStateFlowTest {
+    private lateinit var ratesStateFlow: RatesStateFlow
+    private lateinit var baseCurrencyFlow: BaseCurrencyFlow
+    private lateinit var ratesDaoFake: RatesDaoFake
+
+    @BeforeEach
+    fun setUp() {
+        baseCurrencyFlow = mockk()
+        every { baseCurrencyFlow() } returns flowOf("", "EUR")
+
+        ratesDaoFake = RatesDaoFake()
+
+        ratesStateFlow = RatesStateFlow(
+            baseCurrencyFlow = baseCurrencyFlow,
+            ratesDao = ratesDaoFake
+        )
+    }
+
+    @Test
+    fun `Test rates flow emissions`() = runTest{
+        ratesStateFlow().test {
+            awaitItem()
+
+            val emission1 = awaitItem()
+
+            val overriddenRate = RateUi(
+                from = "EUR",
+                to = "USD",
+                rate = 1.3
+            )
+
+            assertThat(emission1.automatic).doesNotContain(overriddenRate)
+            assertThat(emission1.manual).contains(overriddenRate)
+
+            ratesDaoFake.rates.value += Rate(
+                rate = 0.00004,
+                currency = "BTC"
+            )
+
+            val emission2 = awaitItem()
+
+            val rate = RateUi(
+                from = "EUR",
+                to = "BTC",
+                rate = 0.00004
+            )
+
+            assertThat(emission2.automatic).contains(rate)
+            assertThat(emission2.manual).doesNotContain(rate)
+        }
+    }
+}
